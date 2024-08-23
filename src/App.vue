3089c31bff714d765dadf31a77b05db4fb3269f9
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-08-22 22:07:22 -0300

    feat: HomeView layout and all icons uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,146 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+import { RouterLink, RouterView } from "vue-router";
+import MenuItem from "./components/MenuItem.vue";
+
+let openMenu = ref(false);
+let username = ref("Ricardo André");
+
+let playlists = ref([
+  "My Playlist",
+  "My Playlist",
+  "My Playlist",
+  "My Playlist",
+]);
+
+let menuItems = ref([
+  { name: "Home", iconString: "home", pageUrl: "/", to: "/" },
+  { name: "Search", iconString: "search", pageUrl: "/search", to: "/" },
+  { name: "Your Library", iconString: "library", pageUrl: "/library", to: "/" },
+]);
+</script>
 
 <template>
-  <RouterView />
+  <div>
+    <div
+      id="TopNav"
+      class="w-[calc(100%-240px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
+    >
+      <div class="flex items-center ml-6">
+        <button
+          type="button"
+          class="rounded-full bg-black p-[1px] cursor-pointer"
+        >
+          <ChevronLeft fillColor="#FFFFFF" :size="30" />
+        </button>
+        <button
+          type="button"
+          class="rounded-full bg-black p-[1px] hover:bg-[#] ml-4 cursor-pointer"
+        >
+          <ChevronRight fillColor="#FFFFFF" :size="30" />
+        </button>
+      </div>
+
+      <button
+        @click="openMenu = !openMenu"
+        :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
+        class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+      >
+        <div class="flex items-center">
+          <img
+            class="rounded-full"
+            width="27"
+            src="https://avatars.githubusercontent.com/u/80722562?s=96&v=4"
+          />
+          <div class="text-white text-[14px] ml-1.5 font-semibold">
+            {{ username }}
+          </div>
+          <ChevronDown
+            v-if="!openMenu"
+            @click="openMenu = true"
+            fillColor="#FFFFFF"
+            :size="25"
+          />
+          <ChevronUp
+            v-else
+            @click="openMenu = false"
+            fillColor="#FFFFFF"
+            :size="25"
+          />
+        </div>
+      </button>
+
+      <span
+        v-if="openMenu"
+        class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
+      >
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">
+            Profile
+          </li>
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]">Log out</li>
+        </ul>
+      </span>
+    </div>
+
+    <div id="SideNav" class="h-[100%] p-6 w-[240px] fixed z-50 bg-black">
+      <RouterLink to="/">
+        <img width="125" src="/icons/spotify-logo.png" />
+      </RouterLink>
+
+      <div class="my-8"></div>
+
+      <ul v-for="(item, i) in menuItems" :key="i">
+        <RouterLink :to="item.to">
+          <MenuItem
+            class="ml-[1px]"
+            :iconSize="23"
+            :name="item.name"
+            :iconString="item.iconString"
+            :pageUrl="item.pageUrl"
+          />
+        </RouterLink>
+      </ul>
+
+      <div class="py-3.5"></div>
+
+      <ul>
+        <MenuItem
+          :iconSize="24"
+          name="Create Playlist"
+          iconString="playlist"
+          pageUrl="/playlist"
+        />
+        <MenuItem
+          class="-ml-[1px]"
+          :iconSize="27"
+          name="Liked Songs"
+          iconString="liked"
+          pageUrl="/liked"
+        />
+      </ul>
+
+      <div class="border-b border-b-gray-700"></div>
+
+      <ul v-for="(playlist, i) in playlists" :key="i">
+        <li
+          class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white"
+        >
+          {{ playlist }} #{{ i + 1 }}
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div
+    class="fixed right-0 top-0 w-[calc(100%-240px)] overflow-auto h-full bg-gradient-to-b from-[#1C1C1C] to-black"
+  >
+    <div class="mt-[70px]"></div>
+    <RouterView />
+    <div class="mb-[100px]"></div>
+  </div>
 </template>
